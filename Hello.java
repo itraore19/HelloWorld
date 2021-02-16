fbd6269f58b978d2d6514b5089d4acb501c615cf
Issa Traore <itraore@ece.uvic.ca>
2021-02-15 16:45:56 -0800

    added HelloWorld Program

---
+++ Hello.java
@@ -0,0 +1,13 @@
+//package hello; 
+
+public class Hello {
+
+public static void main(String[] args) {
+
+     for (int i=0; i<=10;i++) {
+        System.out.println("Hellow world..." + i);
+     }
+
+  } 
+
+}
